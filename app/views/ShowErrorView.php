633618252a7b142e87202c84eb4a72319fe6cefb
jeramyw <jeramy.wenserit@strategyeye.com>
2014-07-17 03:06:08 +0200

    tweeks with output

--- app/views/ShowErrorView.php
+++ app/views/ShowErrorView.php
@@ -5,13 +5,17 @@ namespace views;
 class ShowErrorView extends View {
 
     public function executeHtml($dataContainer) {
+
+        // We set the title here, because the title is for HTML ONLY!
+        $dataContainer['title'] = "A HTML error view";
+
         return \View::make('pages/index-error')->with($dataContainer);
     }
 
     public function executeJson($dataContainer) {
 
-        $this->httpStatus = 501;
-        $dataContainer['api-error'] = 'You failed to provide ?show_me';
+        $this->httpStatus = 400;
+        $dataContainer['api-error'] = 'You failed to provide ?show_me=1';
 
         return parent::executeJson($dataContainer);
     }
--- app/views/ShowSuccessView.php
+++ app/views/ShowSuccessView.php
@@ -5,11 +5,16 @@ namespace views;
 class ShowSuccessView extends View {
 
     public function executeHtml($dataContainer) {
+
+        // We set the title here, because the title is for HTML ONLY!
+        $dataContainer['title'] = "A HTML view";
+
         return \View::make('pages/index')->with($dataContainer);
     }
 
     public function executeJson($dataContainer) {
 
+        // We set some API specific data base JSON is for API ONLY!
         $dataContainer['api-data'] = 'Here some extra info just for your API!';
 
         return parent::executeJson($dataContainer);
